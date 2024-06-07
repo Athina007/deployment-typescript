7458ad6aa9401369670a67407996de9007799335
Beanalini <dat826@gmail.com>
2024-06-07 17:01:42 +0100

    edito to header

--- src/client/Header.tsx
+++ src/client/Header.tsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const Header = () => {
   return (
     <header>
-      <h2>Typescript App</h2>
+      <h2>Express Typescript App</h2>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
